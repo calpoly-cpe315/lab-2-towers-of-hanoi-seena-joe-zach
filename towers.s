c8afd8f489fa52124aca6bf6fba276e1fef715c6
ZRiley36 <zachriley36@gmail.com>
2022-02-10 20:10:15 -0800

    attempted to fill out towers.s functions

--- towers.s
+++ towers.s
@@ -1,8 +1,8 @@
-	.arch armv8-a
-	.text
-
+   .arch armv8-a
+   .text
+ 
 @ print function is complete, no modifications needed
-    .global	print
+    .global print
 print:
       stp    x29, x30, [sp, -16]! //Store FP, LR.
       add    x29, sp, 0
@@ -13,59 +13,79 @@ print:
       bl     printf
       ldp    x29, x30, [sp], 16
       ret
-
+ 
 startstring:
-	.word	string0
-
-    .global	towers
+   .word string0
+ 
+    .global towers
 towers:
    /* Save callee-saved registers to stack. We will need 5 so
       we save x19-x22 and w19 also we save the return address*/
       sub sp, sp, 64
       stp x30, [sp]
-      stp x19, [sp, 8]
-      stp x20, [sp, 16]
-      stp x21, [sp, 24]
-      stp x22, [sp, 32]
-      stp w19, [sp, 40]
+      stp x19, [sp, 8]  /*start*/
+      stp x20, [sp, 16] /*goal*/
+      stp x21, [sp, 24] /*temp*/
+      stp x22, [sp, 32] /*total-steps*/
+      stp w19, [sp, 40] /*numDisks*/
    
    /* Save a copy of all 3 incoming parameters to callee-saved registers */
       ldr w19, w0 /* numDisks */
       ldr x19, x1 /* Start */
       ldr x20, x2 /* Goal */
-      
+     
 if:
    /* Compare numDisks with 2 or (numDisks - 2)*/
-      cmp [w19, -2], 0 
+      cmp w19, 1
    /* Check if less than, else branch to else */
       bne else
    /* set print function's start to incoming start */
+      ldr x0, x19
    /* set print function's end to goal */
+      ldr x1, x20
    /* call print function */
+      bl print
    /* Set return register to 1 */
+      ldr x1, 1
    /* branch to endif */
+      b endif
+ 
+ 
 else:
-   /* Use a callee-saved varable for temp and set it to 6 */
-   /* Subract start from temp and store to itself */
+   /* Use a callee-saved variable for temp and set it to 6 */
+      ldr x21, 6
+   /* Subtract start from temp and store to itself */
+      sub x21, x21, x19
    /* Subtract goal from temp and store to itself (temp = 6 - start - goal)*/
-
-   /* subtract 1 from original numDisks and store it to numDisks parameter */
-
+      sub x21, x21, x20
+   /*subtract 1 from original numDisks and store it to numDisks parameter */
+      sub w0, w19, 1
    /* Set end parameter as temp */
+      ldr x2, x21
    /* Call towers function */
+      bl towers
    /* Save result to callee-saved register for total steps */
+      ldr x22,x1
    /* Set numDiscs parameter to 1 */
+      ldr w0, 1
    /* Set start parameter to original start */
+      ldr x1, x19
    /* Set goal parameter to original goal */
+      ldr x2, x20	
    /* Call towers function */
+      bl towers
    /* Add result to total steps so far */
    
    /* Set numDisks parameter to original numDisks - 1 */
+      ldr w0, w20
    /* set start parameter to temp */
+      ldr x1, x21
    /* set goal parameter to original goal */
+      ldr x2, x20
    /* Call towers function */
+      bl towers
    /* Add result to total steps so far and save it to return register */
-
+      add x1, x1, x22
 endif:
    /* Restore Registers */
       ldr x30, [sp]
@@ -75,17 +95,17 @@ endif:
       ldr x22, [sp, 32]
       ldr w19, [sp, 40]
       add sp, sp, 64
-
+ 
    /* Return from towers function */
-      ret     
-
-
+      ret    
+ 
+ 
 @ Function main is complete, no modifications needed
-    .global	main
+    .global main
 main:
       stp    x29, x30, [sp, -32]!
       add    x29, sp, 0
-      ldr    w0, printdata 
+      ldr    w0, printdata
       bl     printf
       ldr    w0, printdata + 4
       add    x1, x29, 28
@@ -104,19 +124,19 @@ main:
       ldp    x29, x30, [sp], 16
       ret
 end:
-
+ 
 printdata:
-	.word	string1
-	.word	string2
-	.word	string3
-
+   .word string1
+   .word string2
+   .word string3
+ 
 string0:
-	.asciz	"Move from peg %d to peg %d\n"
+   .asciz   "Move from peg %d to peg %d\n"
 string1:
-	.asciz	"Enter number of discs to be moved: "
+   .asciz   "Enter number of discs to be moved: "
 string2:
-	.asciz	"%d"
-	.space	1
+   .asciz   "%d"
+   .space   1
 string3:
-	.ascii	"\n%d discs moved from peg %d to peg %d in %d steps."
-	.ascii	"\012\000"
+   .ascii   "\n%d discs moved from peg %d to peg %d in %d steps."
+   .ascii   "\012\000"
